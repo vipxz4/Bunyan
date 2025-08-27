568d112c02ee68989d20eb788a20bd5f84e0e723
vipxz4 <vip4xz4@gmail.com>
2025-08-28 01:46:18 +0300

    رسالتك الوصفبيية هنا

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 dependencies {
   // Import the Firebase BoM
-  implementation platform('com.google.firebase:firebase-bom:34.1.0')
+   implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
 
 
   // TODO: Add the dependencies for Firebase products you want to use
